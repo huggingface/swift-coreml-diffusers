507d76998b5d2733740269af775fdb26e898bd5c
Pedro Cuenca <pedro@huggingface.co>
2023-01-18 12:33:18 +0100

    Simplify code

--- Diffusion-macOS/GeneratedImageView.swift
+++ Diffusion-macOS/GeneratedImageView.swift
@@ -22,26 +22,15 @@ struct GeneratedImageView: View {
             let fraction = Double(step) / Double(progress.stepCount)
             let label = "Step \(step) of \(progress.stepCount)"
             return AnyView(ProgressView(label, value: fraction, total: 1).padding())
-        case .complete(let lastPrompt, let image, let interval):
+        case .complete(_, let image, _):
             guard let theImage = image else {
                 return AnyView(Image(systemName: "exclamationmark.triangle").resizable())
             }
                               
-            let imageView = Image(theImage, scale: 1, label: Text("generated"))
-            return AnyView(
-                VStack {
-                    imageView.resizable().clipShape(RoundedRectangle(cornerRadius: 20))
-//                    HStack {
-//                        let intervalString = String(format: "Time: %.1fs", interval ?? 0)
-//                        Rectangle().fill(.clear).overlay(Text(intervalString).frame(maxWidth: .infinity, alignment: .leading).padding(.leading))
-//                        Rectangle().fill(.clear).overlay(
-//                            HStack {
-//                                Spacer()
-//                                ShareButtons(image: theImage, name: lastPrompt).padding(.trailing)
-//                            }
-//                        )
-//                    }.frame(maxHeight: 25)
-            })
+            return AnyView(Image(theImage, scale: 1, label: Text("generated"))
+                .resizable()
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            )
         }
     }
 }
