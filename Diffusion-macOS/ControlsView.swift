6e6ab20645e159f22a96afae57baae48e05abb1b
Pedro Cuenca <pedro@huggingface.co>
2023-01-23 18:38:07 +0100

    Reference LICENSE in all files.

--- Diffusion-macOS/ControlsView.swift
+++ Diffusion-macOS/ControlsView.swift
@@ -3,6 +3,7 @@
 //  Diffusion-macOS
 //
 //  Created by Cyril Zakka on 1/12/23.
+//  See LICENSE at https://github.com/huggingface/swift-coreml-diffusers/LICENSE
 //
 
 import Combine
--- Diffusion-macOS/GeneratedImageView.swift
+++ Diffusion-macOS/GeneratedImageView.swift
@@ -3,6 +3,7 @@
 //  Diffusion
 //
 //  Created by Pedro Cuenca on 18/1/23.
+//  See LICENSE at https://github.com/huggingface/swift-coreml-diffusers/LICENSE
 //
 
 import SwiftUI
