11e20ac9c37695a96b5acfca332f78cdb6b71395
dolmere <47501428+dolmere@users.noreply.github.com>
2023-06-24 14:28:20 +0200

    Update Diffusion/Common/Pipeline/PipelineLoader.swift
    remove comment
    
    Co-authored-by: Pedro Cuenca <pedro@huggingface.co>

--- Diffusion/Common/Pipeline/PipelineLoader.swift
+++ Diffusion/Common/Pipeline/PipelineLoader.swift
@@ -114,7 +114,6 @@ extension PipelineLoader {
         }
     }
     
-    // TODO: maybe receive Progress to add another progress as child --pcuenca
     func prepare() async throws -> Pipeline {
         do {
             do {
