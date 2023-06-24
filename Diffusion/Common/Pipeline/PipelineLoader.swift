cf765f2cbf61d6cf1acb628485dcf6ec831a0903
dolmere <47501428+dolmere@users.noreply.github.com>
2023-06-24 14:23:36 +0200

    Update Diffusion/Common/Pipeline/PipelineLoader.swift
    remove debugging comments
    
    Co-authored-by: Pedro Cuenca <pedro@huggingface.co>

--- Diffusion/Common/Pipeline/PipelineLoader.swift
+++ Diffusion/Common/Pipeline/PipelineLoader.swift
@@ -116,15 +116,6 @@ extension PipelineLoader {
     
     // TODO: maybe receive Progress to add another progress as child --pcuenca
     func prepare() async throws -> Pipeline {
-//        // Check that the constructed locations match the expected locations
-//        print("url: \(url.path)")
-//        print("filename \(filename)")
-//        print("downloadedURL \(downloadedURL.path)")
-//        print("uncompressURL \(uncompressURL.path)")
-//        print("packagesFilename \(packagesFilename)")
-//        print("compiledURL \(compiledURL.path)")
-//        print("downloaded \(downloaded)")
-//        print("ready \(ready)")
         do {
             // Make default models folder if missing
             let fileExists = FileManager.default.fileExists(atPath: PipelineLoader.models.path)
